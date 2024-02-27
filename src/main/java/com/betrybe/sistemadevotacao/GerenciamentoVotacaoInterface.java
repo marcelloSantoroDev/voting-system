f0cdbb4402e0c87d6d2870777a44457fdbb60d18
Marcello <marcelloaltermundi@gmail.com>
2024-02-27 15:31:50 -0300

    starting project

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacaoInterface.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacaoInterface.java
@@ -5,11 +5,32 @@ package com.betrybe.sistemadevotacao;
  */
 public interface GerenciamentoVotacaoInterface {
 
+  /**
+   * Cadastrar pessoa candidata.
+   *
+   * @param nome   the nome
+   * @param numero the numero
+   */
   void cadastrarPessoaCandidata(String nome, int numero);
 
+  /**
+   * Cadastrar pessoa eleitora.
+   *
+   * @param nome the nome
+   * @param cpf  the cpf
+   */
   void cadastrarPessoaEleitora(String nome, String cpf);
 
+  /**
+   * Votar.
+   *
+   * @param cpfPessoaEleitora     the cpf pessoa eleitora
+   * @param numeroPessoaCandidata the numero pessoa candidata
+   */
   void votar(String cpfPessoaEleitora, int numeroPessoaCandidata);
 
+  /**
+   * Mostrar resultado.
+   */
   void mostrarResultado();
 }
